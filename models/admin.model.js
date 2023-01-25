aa93ab644a98bcbe90a1febf334325e37f8c6f64
thereseml <tm-l@live.se>
2023-01-25 13:04:28 +0100

    change admin

--- models/admin.model.js
+++ models/admin.model.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
 const adminSchema = mongoose.Schema({
-  useremail: { type: String, required: true, unique: true },
+  adminemail: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
